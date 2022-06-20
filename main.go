8432b9299f1da267673f624527613ea1e83d47f2
feilongjump <feilongjump@qq.com>
2022-06-20 22:09:14 +0800

    update

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	// 配置初始化，依赖命令行 --env 参数
 	var env string
 	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
-	// flag.Parse()
+	flag.Parse()
 	config.InitConfig(env)
 
 	// new 一个 Gin Engine 实例

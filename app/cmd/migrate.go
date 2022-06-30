b4774bf7af399913c4c174952916f71a24b56ee7
feilongjump <feilongjump@qq.com>
2022-06-30 17:53:04 +0800

    migrate reset 和 fresh 命令

--- app/cmd/migrate.go
+++ app/cmd/migrate.go
@@ -26,10 +26,24 @@ var CmdMigrateRollback = &cobra.Command{
 	Run:     runDown,
 }
 
+var CmdMigrateReset = &cobra.Command{
+	Use:   "reset",
+	Short: "Rollback all database migrations",
+	Run:   runReset,
+}
+
+var CmdMigrateRefresh = &cobra.Command{
+	Use:   "refresh",
+	Short: "Reset and re-run all migrations",
+	Run:   runRefresh,
+}
+
 func init() {
 	CmdMigrate.AddCommand(
 		CmdMigrateUp,
 		CmdMigrateRollback,
+		CmdMigrateRefresh,
+		CmdMigrateReset,
 	)
 }
 
@@ -47,4 +61,12 @@ func runUp(cmd *cobra.Command, args []string) {
 
 func runDown(cmd *cobra.Command, args []string) {
 	migrator().Rollback()
+}
+
+func runReset(cmd *cobra.Command, args []string) {
+	migrator().Reset()
+}
+
+func runRefresh(cmd *cobra.Command, args []string) {
+	migrator().Refresh()
 }
